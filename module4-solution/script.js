94c972817a642c53c75b5d91a4ebd412b8e5661e
Saqib ali <saqibalilogan223@gmail.com>
2021-07-13 11:34:58 +0500

    assignment 4

---
+++ module4-solution/script.js
@@ -0,0 +1,14 @@
+(function () {
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (/*fill in parts of the 'for' loop to loop over names array */var i = 0; i < names.length; i++){
+
+var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (/*fill in condition here*/ firstLetter == 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+})();
